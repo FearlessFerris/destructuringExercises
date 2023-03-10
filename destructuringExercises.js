ec3e021c3e8e22576cd51e1bee26c5ae54793972
Marcus Perry <marcus.perry2017@gmail.com>
2023-03-09 19:36:43 -0500

    Add files via upload

---
+++ destructuringExercises.js
@@ -0,0 +1,75 @@
+// Object Destructuring 1
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+
+
+
+// Object Destructuring 2
+
+console.log(discoveryYears);
+// { yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659 }
+
+
+
+
+
+// Object Deconstructing 3
+
+// Your name is Alejandro and you like purple
+// Your name is Melissa and you like green
+// Your name is undefined and you like green
+
+
+
+
+
+// Array Destructuring 1
+
+// Maya 
+// Marisa 
+// Chi 
+
+
+
+
+
+// Array Destructuring 2
+
+// 'Raindrops on roses'
+// 'whiskers on kittens'
+// ['B'right copper kettles, 'warm woolen mittens', 'Brown paper packages tied up with strings']
+
+
+
+
+
+// Array Destructuring 3
+
+// [10, 20, 30]
+
+
+
+
+
+// ES5 Assigning Variables to Object Properties
+
+// const {a, b} = obj.numbers
+
+
+
+
+
+// ES5 Array Swap
+
+// [ arr[0], arr[1] ] = [ arr[1], arr[0] ]
+
+
+
+
+
+// raceResults 
+
+//const raceResults = ([ first, second, third, ...rest ]) => ({ first, second, third, rest });
+
